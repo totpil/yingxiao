2e7f1603910f2167f012d03f94f861ee8e112e36
TotPil <846685686@qq.com>
2020-01-01 01:20:04 +0800

    node okay

--- nodejs/nodejs.js
+++ nodejs/nodejs.js
@@ -1,38 +1,31 @@
 ﻿var http = require('http');
+var mysql = require('mysql');
 
-function mysqlask(){
-  var mysql = require('mysql');
-  var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'nodejs',
     password : 'nodejs',
-    database : 'dmcnew'
-  });
-
+    database : 'test'
+});
   connection.connect();
 
-
+var server = http.createServer(function(req,res){
   var  sql = 'SELECT * FROM test';
-
   connection.query(sql,function (err, result) {
     if(err){
       console.log('[SELECT ERROR] - ',err.message);
       return;
     }
-
-  console.log('--------------------------SELECT----------------------------');
-  console.log(result);
-  console.log('------------------------------------------------------------\n\n');  
+    console.log('--------------------------SELECT----------------------------');
+    var str = JSON.stringify(result[0]);
+    console.log(str);res.write(str);
+    console.log('------------------------------------------------------------\n\n');  
+    res.end()
   });
-}
 
-
-var server = http.createServer(function(req,res){
-    mysqlask();
     res.setHeader("Access-Control-Allow-Origin", "*"); 
     res.writeHead(200, {"Content-Type" : "text/plain; charset=utf-8"});
-    res.write("hello node.js!");
-    res.end()
+
 })
 
 server.listen(3000)
