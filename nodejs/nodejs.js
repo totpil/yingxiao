774d17be0e3eb34708419ebfabd69136caeff8df
TotPil <846685686@qq.com>
2020-01-01 22:41:27 +0800

    http请求加上

--- nodejs/nodejs.js
+++ nodejs/nodejs.js
@@ -16,17 +16,39 @@ var server = http.createServer(function(req,res){
 
 
   
-  var  sql = 'SELECT * FROM test';
-  connection.query(sql,function (err, result) {
-    if(err){
-      console.log('[SELECT ERROR] - ',err.message);
-      return;
+  console.log("get a user");
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;  //一定要使用+=，如果body=chunk，因为请求favicon.ico，body会等于{}
+    console.log("chunk:",chunk);
+  });
+  req.on('end', function () {
+    body = querystring.parse(body);
+    console.log("body:",body);
+    if(body.tj){
+      console.log(body.tj);console.log("tj exists");
+      var par = body.tj;
+      var sql = 'SELECT * FROM test where tj = ?'
+      console.log('SELECT * FROM test where tj = '+body.tj);
+      connection.query(sql,par,function (err,result) {
+        if(err||result === undefined){
+          console.log('[SELECT ERROR] - ',err.message);
+          res.end()
+          return;
+        }else{
+          console.log('--------------------------SELECT----------------------------');
+          console.log(result[0]);
+          var str = JSON.stringify(result[0]);
+          console.log(str);
+          if (str === undefined){res.write("{\"name\":\"根据工号未查询到人员\"}");}else{res.write(str);}
+          console.log('------------------------------------------------------------\n\n');  
+          res.end()
+        }
+      });
+    }else{
+      res.write("{name:'根据工号未查询到人员'}");
+      res.end();
     }
-    console.log('--------------------------SELECT----------------------------');
-    var str = JSON.stringify(result[0]);
-    console.log(str);res.write(str);
-    console.log('------------------------------------------------------------\n\n');  
-    res.end()
   });
 
     res.setHeader("Access-Control-Allow-Origin", "*"); 
